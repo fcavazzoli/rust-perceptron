d4eed48a8cad0a278793fc1512a276d60080dc0e
Federico <fedecavazzoli@gmail.com>
2023-01-06 11:14:11 -0300

    truncate fn for test

--- tests/perceptron.rs
+++ tests/perceptron.rs
@@ -2,6 +2,12 @@
 
 use rust_perceptron::perceptron::Perceptron;
 
+fn truncate(number: f64) -> f64 {
+    let truncated = format!("{:.5}", number);
+    let truncated_number: f64 = truncated.parse().unwrap();
+    truncated_number
+}
+
 #[test]
 fn test_perceptron() {
     let mut p = Perceptron::new(2, 0.1);
@@ -16,18 +22,9 @@ fn test_perceptron() {
         p.train(inputs, *target, Some(6));
     }
 
-    let truncated = format!("{:.5}", p.bias);
-    let truncated_bias: f64 = truncated.parse().unwrap();
-
-    let weight_0 = format!("{:.5}", p.weights[0]);
-    let truncated_weight_0: f64 = weight_0.parse().unwrap();
-
-    let weight_1 = format!("{:.5}", p.weights[1]);
-    let truncated_weight_1: f64 = weight_1.parse().unwrap();
-
-    assert_eq!(truncated_bias, 0.17720);
-    assert_eq!(truncated_weight_0, 0.16720);
-    assert_eq!(truncated_weight_1, 0.17820);
+    assert_eq!(truncate(p.bias), 0.17720);
+    assert_eq!(truncate(p.weights[0]), 0.16720);
+    assert_eq!(truncate(p.weights[1]), 0.17820);
     assert_eq!(p.predict(&[0.0, 0.0]), 1.0);
     assert_eq!(p.predict(&[0.0, 1.0]), 1.0);
     assert_eq!(p.predict(&[1.0, 0.0]), 1.0);
