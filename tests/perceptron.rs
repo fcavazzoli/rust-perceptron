8a47883fe0936a4a1b1e4730a0ccf85a656c828e
Federico <fedecavazzoli@gmail.com>
2023-01-06 11:17:55 -0300

    add decimals as parameter in truncate fn

--- tests/perceptron.rs
+++ tests/perceptron.rs
@@ -2,8 +2,8 @@
 
 use rust_perceptron::perceptron::Perceptron;
 
-fn truncate(number: f64) -> f64 {
-    let truncated = format!("{:.5}", number);
+fn truncate(number: f64, decimals: usize) -> f64 {
+    let truncated = format!("{:.1$}", number, decimals);
     let truncated_number: f64 = truncated.parse().unwrap();
     truncated_number
 }
@@ -22,9 +22,9 @@ fn test_perceptron() {
         p.train(inputs, *target, Some(6));
     }
 
-    assert_eq!(truncate(p.bias), 0.17720);
-    assert_eq!(truncate(p.weights[0]), 0.16720);
-    assert_eq!(truncate(p.weights[1]), 0.17820);
+    assert_eq!(truncate(p.bias, 5), 0.17720);
+    assert_eq!(truncate(p.weights[0], 5), 0.16720);
+    assert_eq!(truncate(p.weights[1], 5), 0.17820);
     assert_eq!(p.predict(&[0.0, 0.0]), 1.0);
     assert_eq!(p.predict(&[0.0, 1.0]), 1.0);
     assert_eq!(p.predict(&[1.0, 0.0]), 1.0);
